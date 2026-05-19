7dc52cd65d3e4e9e4be1048fb1ed0189f8688355
neuza701 <neuzachen@tecnico.ulisboa.pt>
2026-05-19 22:54:34 +0100

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,35 @@
+class Graph():
+    def __init__(self,n):
+        self.matrix=[[0 for i in range(n)] for j in range(n)]
+
+    def nnodes(self):
+        return len(self.matrix)
+
+    def add(self,i,j,w):
+        assert 0<= i< self.nnodes() and 0<=j< self.nnodes()
+        self.matrix[i][j]=w
+
+    def adjQ(self,i,j):
+        assert 0<= i< self.nnodes() and 0<=j< self.nnodes()
+        return self.matrix[i][j]>0
+
+    def adj(self,i):
+        #ou return[d for d in range(self.nnodes()) if self.adjQ(i,d)]
+        r=[]
+        for j in range(len(self.matrix)):
+            if self.matrix[i][j]>0:
+                r=r+[j]
+        return r
+        
+    def __str__(self):
+        return str(self.matrix)
+        
+    def weight(self,i,j):
+        return self.matrix[i][j]
+
+    def totalWeight(self):
+        r=0
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                r=r+self.matrix[i][j]
+        return r   
